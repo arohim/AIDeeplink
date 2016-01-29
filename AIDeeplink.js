97e9bc4b79fab056b2e6989e2e304f2a0dde8c72
unknown <AbdulrohimS@GAME-DEV1-PC.sanookonline.co.th>
2016-01-29 13:41:41 +0700

    initial

---
+++ AIDeeplink.js
@@ -0,0 +1,30 @@
+/**
+ * Created by AbdulrohimS on 2016/1/29.
+ */
+AIDeeplink = window.AIDeeplink || {};
+
+AIDeeplink = function () {
+
+    var isSupportBrowser = [];
+    var options;
+
+    setup = function (options) {
+
+    };
+
+    openWithCallback = function () {
+
+    };
+
+    openApp = function () {
+
+    };
+
+
+    return {
+        "setup": setup,
+        "openWithCallback": openWithCallback,
+        "openApp": openApp
+    }
+
+}();
